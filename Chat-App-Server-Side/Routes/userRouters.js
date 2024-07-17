80f6b379f98c8a3dafab266986ea43ee415db96b
sid773 <siddu10.klm@gmail.com>
2024-07-17 20:08:43 +0530

    change

--- Chat-App-Server-Side/Routes/userRouters.js
+++ Chat-App-Server-Side/Routes/userRouters.js
@@ -3,7 +3,7 @@ const express = require("express");
 const Router=express.Router();
 
 
-Router.post("/login",loginController);
-Router.post("/regster",registerController);
+// Router.post("/login",loginController);
+// Router.post("/regster",registerController);
 
 module.exports = Router
