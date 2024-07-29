0695c5be30e485959681a7a0825faa980c07cf1d
sid773 <siddu10.klm@gmail.com>
2024-07-29 20:20:12 +0530

    live

--- Chat-App-Client-Side/src/Components/Groups.js
+++ Chat-App-Client-Side/src/Components/Groups.js
@@ -114,6 +114,16 @@ function Groups() {
                   dispatch(refreshSidebarFun());
                 }}
               >
+              <motion.div
+                whileHover={{ scale: 1.01 }}
+                whileTap={{ scale: 0.98 }}
+                className={"list-item" + (lightTheme ? "" : " dark")}
+                key={index}
+                onClick={() => {
+                  console.log("Creating chat with group", group.name);
+                  dispatch(refreshSidebarFun());
+                }}
+              >
                 <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
                 <p className={"con-title" + (lightTheme ? "" : " dark")}>
                   {group.chatName}
